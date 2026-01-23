e3b5df1194ed50b6ee108a42fc05d16fdf428d25
Evan Owens <eowens9516@floridapoly.edu>
2026-01-23 21:49:54 +0000

    list

---
+++ 02_SLList/SLL_test.cpp
@@ -0,0 +1,14 @@
+#include "SLList.hpp"
+
+int main()
+{
+    SLList list;
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+    list.push_front(4);
+
+    list.print();
+    
+    return 0;
+}
